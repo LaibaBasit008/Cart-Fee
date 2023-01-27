f0a0de42d70cecf790a42df142e7760742324278
LaibaBasit008 <laibabasit22@gmail.com>
2023-01-28 01:27:44 +0500

    Json Payload

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import *
+import json, time
+from deliveryfee import deliveryFee
+app = Flask(__name__)
+@app.route('/')
+@app.route('/user',methods=['GET'])
+def calculateDeliveryFee():
+   
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        data = request.json
+        cartValue=data.get('cart_value')
+        deliveryDistance=data.get('cart_value')
+        noOfItems=data.get('number_of_items')
+        time=data.get('time')
+        deliveryCost=deliveryFee(cartValue,deliveryDistance,noOfItems,time)
+        
+        
+
+    return {"delivery_fee":deliveryCost}
+    
+if __name__ =='__main__':
+    app.run()
+    
---
+++ deliveryfee.py
@@ -0,0 +1,4 @@
+def deliveryFee(cartValue,deliveryDistance,noOfItems,time):
+    
+    deliveryFee=0
+    return deliveryFee
